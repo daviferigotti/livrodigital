8154b0fcc22d4745ccad933dc3edcfdbe0d3cd1d
Davi Ferigotti <davi.lferigotti@gmail.com>
2018-06-12 17:48:09 -0300

    Novas funcionalidades //menu com opções

--- public/js/dropdown-menu.js
+++ public/js/dropdown-menu.js
@@ -13,40 +13,32 @@ $(document).ready(function(){
       $('.materialboxed').materialbox();
 
       $('.collapsible').collapsible();
+
+      $('.modal').modal();
 });
-
-
 
 //chamando paralax
 function chamaParallax(){
   $('.parallax').parallax();
 };
-
 window.load = chamaParallax();
 
 //daqui para frente é código para fazer animações sem o materialize
 //scroll para ancoras deslizarem devagar usando jquery
-
-
-
 $('.scroll').click(function(event) {
      event.preventDefault();
      var id = $(this).attr('href');
      targetOffset = $(id).offset().top;
      menuHeight = $('.scroll').innerHeight();
      $('html, body').animate({scrollTop:targetOffset - menuHeight}, 1000)
-
    });
 
+//campos de autoatividade congelarem ao mostrar resposta
+$("#ebibir-resposta-1").click(exibeResposta(textarea1));
 
-var campoDeResposta = $(".autoatividade");
-botaoExibeResposta = $(".mostra-resposta-discursiva");
-botaoExibeResposta.click(function(event){
-  var id = $(this).attr('href');
-  id.attr("disabled", true);
-  id.toggleClass("campo-desativado");
-});
-
-function confirmaResposta(){
-
+function exibeResposta(id){
+  var idCampo = "#" + id;
+  var campo = $(campo);
+  campo.attr("disabled", true);
+  campo.addClass("campo-desativado");
 }
